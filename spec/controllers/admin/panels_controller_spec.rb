966a1769708d87290b6cd7f86219ae1e04b24b9a
Dariusz Garbarz <hy37id@gmail.com>
2015-03-14 13:55:10 +0100

    Added spec admin/panel controller

--- spec/controllers/admin/panels_controller_spec.rb
+++ spec/controllers/admin/panels_controller_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 RSpec.describe Admin::PanelsController, type: :controller do
 
+  let(:admin) { FactoryGirl.build(:admin) }
+  #admin_user = User.new(email: Faker::Internet.email,encrypted_password: Faker::Internet.password, admin:true )
+
+  it "allowed only to admin" do
+    expect(admin).to be_admin
+  end
+
+
 end
