4b48c7de32b0636df6e44510a274ea4da40b4656
kkulikowski <kontakt@internetowakuznia.pl>
2015-03-14 15:58:16 +0100

    Add Product filtering

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     root to: "panels#index"
     resources :users
   end
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
