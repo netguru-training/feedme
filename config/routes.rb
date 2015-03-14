fdc6e6bd8a4fba21278876bdbb91b02f3fa84f09
kkulikowski <kontakt@internetowakuznia.pl>
2015-03-14 16:01:38 +0100

    Add Product filtering

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :products
   # You can have the root of your site routed with "root"
-  root 'foods#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
