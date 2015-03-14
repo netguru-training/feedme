9e36ea0bdaaee6ffb3172c28c974b5ea060619d1
barstuk <barstuk.ror@gmail.com>
2015-03-14 13:53:10 +0100

    added open uri

--- app/services/food_fetcher/grzesznicy.rb
+++ app/services/food_fetcher/grzesznicy.rb
@@ -1,4 +1,14 @@
+require 'open-uri'
+
 module FoodFetcher
   class Grzesznicy
+
+    def fetch_food
+      # binding.pry
+      page = Nokogiri::HTML(open("http://www.grzesznicy.eu/menu.html"))
+      # binding.pry
+      2+2
+    end
+
   end
 end
--- app/services/food_fetcher/pyszne.rb
+++ app/services/food_fetcher/pyszne.rb
@@ -1,4 +1,10 @@
+require 'open-uri'
+
 module FoodFetcher
   class Pyszne
+
+    def fetch_food
+    end
+
   end
 end
