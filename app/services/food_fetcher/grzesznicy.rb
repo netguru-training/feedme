26d583e604dc910ed5f7a9f9270d5253f1f8efb9
barstuk <barstuk.ror@gmail.com>
2015-03-14 14:27:37 +0100

    added travis, fixed app

--- app/services/food_fetcher/grzesznicy.rb
+++ app/services/food_fetcher/grzesznicy.rb
@@ -3,12 +3,17 @@ require 'open-uri'
 module FoodFetcher
   class Grzesznicy
 
-    def fetch_food
-      binding.pry
-      page = Nokogiri::HTML(open("http://www.grzesznicy.eu/menu.html"))
-      binding.pry
-      2+2
-    end
+    # def fetch_food
+    #   page = Nokogiri::HTML(open("http://www.grzesznicy.eu/menu.html"))
+    #   page.css('tr').each do | product | do
+
+
+    #     end
+    #     name = page.css('tr').first.css('strong').text
+    #     cena = page.css('tr').first.css('td')[1].text.to_i
+
+    #   end
+    # end
 
   end
 end
