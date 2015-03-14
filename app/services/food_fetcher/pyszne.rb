df5715949c3c383c6edd9b1bc3a639db27b922fd
Arek Ś <aroooo@poczta.fm>
2015-03-14 14:41:15 +0100

    fetch pizza from pyszne pl

--- app/services/food_fetcher/pyszne.rb
+++ app/services/food_fetcher/pyszne.rb
@@ -1,4 +1,14 @@
+require 'open-uri'
+
 module FoodFetcher
   class Pyszne
+    def fetch_food
+      page = Nokogiri::HTML(open("https://pyszne.pl/pracownia-pizzy-rzeszow#!"))
+      page.css('div.yd-latebinding.yd-jig-menu-meal-add.tr').each do |tr_position|
+        name = tr_position.children[3].text.scan(/\w+/).join(' ')
+        price = page.css('div.yd-latebinding.yd-jig-menu-meal-add.tr')[0].children[3].text.match(/\d+.\d+/) 
+        Product.create(price: price, name: name)
+      end 
+    end
   end
 end
