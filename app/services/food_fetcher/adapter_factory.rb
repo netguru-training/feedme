0670bb614ecb2a3f49f7246933fc534d0cafe4eb
barstuk <barstuk.ror@gmail.com>
2015-03-14 12:54:09 +0100

    made better adapter

--- app/services/food_fetcher/adapter_factory.rb
+++ app/services/food_fetcher/adapter_factory.rb
@@ -2,14 +2,7 @@ module FoodFetcher
   class AdapterFactory
 
     def return_adapter(name)
-      case name
-      when "grzesznicy" then
-        FoodFetcher::Grzesznicy.new
-      when "pyszne"
-        FoodFetcher::Pyszne.new
-      else
-        raise "Missing adapter for name #{name}"
-      end
+      FoodFetcher.const_get(name.capitalize).new rescue raise "Missing adapter for name #{name}"
     end
 
   end
