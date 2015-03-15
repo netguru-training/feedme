43876b980ca32e452a63462d745c88de6c4c4571
barstuk <barstuk.ror@gmail.com>
2015-03-15 14:44:31 +0100

    tested product model method

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#favourite" do
+    before do
+      @product = create(:product)
+      @user = create(:user)
+    end
+    it "returns true if product is marked as favourite by user" do
+      create(:favourite_product, product: @product, user: @user)
+      expect(@product.favourite(@user.id)).to eq true
+    end
+    it "returns false if product is not marked as favourite by user" do
+      expect(@product.favourite(@user.id)).to eq false
+    end
+  end
 end
